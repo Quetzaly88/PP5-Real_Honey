195080255a6b321e05534ae8dfd4a9f080d1c5aa
Andrea Nilsson <andreaycoral@icloud.com>
2025-02-03 23:05:45 +0000

    - Implemented product_list.html to display products with images, names, and prices - Added sorting functionality (by name and price) - Integrated pagination for better navigation - Created Product model with ImageField support - Fixed Pillow installation issue for handling images

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
+def product_list(request):
+    sort_by = request.GET.get('sort_by', 'name') # default sort by name
+    products = Product.objects.all().order_by(sort_by)
+
+    paginator = Paginator(products, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'products/product_list.html', {'page_obj': page_obj})
