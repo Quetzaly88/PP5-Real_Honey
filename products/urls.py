bbb249ebe4b17882c63c288f88bbe3338ce8263a
Andrea Nilsson <andreaycoral@icloud.com>
2025-02-04 18:45:10 +0000

    Implement Prosuct detail page with pagination, sorting and detail view. Erase redundant products in rear_honey app/

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import product_list, product_detail
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('<int:pk>/', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -4,11 +4,17 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def product_list(request):
-    sort_by = request.GET.get('sort_by', 'name') # default sort by name
-    products = Product.objects.all().order_by(sort_by)
-
-    paginator = Paginator(products, 6)
+    sort_by = request.GET.get('sort_by', 'name')
+    products = Product.objects.all().order_by(sort_by)  
+    
+    # Pagination
+    from django.core.paginator import Paginator
+    paginator = Paginator(products, 6) # Show 6 products per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     
-    return render(request, 'products/product_list.html', {'page_obj': page_obj})
+    return render(request, 'products/product_list.html', {'products': page_obj})
+
+def produc_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'products/product_detail.html', {'product': product})
