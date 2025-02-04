cc26740c71eff68eb29e261b30871da848b4811c
Andrea Nilsson <andreaycoral@icloud.com>
2025-02-04 19:02:42 +0000

    Correct typo in product_detail view and update ULR routing.

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from .views import product_list, product_detail
+from .views import product_list, product_detail 
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('<int:pk>/', views.product_detail, name='product_detail'),
+    path('', product_list, name='product_list'),
+    path('<int:pk>/', product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.core.paginator import Paginator
 
@@ -7,14 +7,12 @@ def product_list(request):
     sort_by = request.GET.get('sort_by', 'name')
     products = Product.objects.all().order_by(sort_by)  
     
-    # Pagination
-    from django.core.paginator import Paginator
     paginator = Paginator(products, 6) # Show 6 products per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     
     return render(request, 'products/product_list.html', {'products': page_obj})
 
-def produc_detail(request, pk):
+def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'products/product_detail.html', {'product': product})
