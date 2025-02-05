42c93f23a5cff48c91d9d80dfad0850d245de1c3
Andrea Nilsson <andreaycoral@icloud.com>
2025-02-05 08:43:11 +0000

    Add optional image display for testing and render default image in case the product is missing image

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 navbar: https://getbootstrap.com/docs/4.0/components/navbar/
 
 Bootstrap 4 Card container: https://bbbootstrap.com/snippets/card-container-48980697
+
+bootstrap pagination: https://getbootstrap.com/docs/4.0/components/pagination/
--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/', blank=True, null=True) #make image optional
     
     def __str__(self):
         return self.name
