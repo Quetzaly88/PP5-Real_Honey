ec010f4fed1325fd6be6559954740ea62befd2fc
Andrea Nilsson <andreaycoral@icloud.com>
2025-02-10 18:14:46 +0000

    Create a combined template for wishlist and cart items. Correct typos and bad use of else in cart.html

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import wishlist_view, add_to_wishlist, remove_from_wishlist
+from .views import wishlist_view, add_to_wishlist, remove_from_wishlist, combined_view 
+
 
 urlpatterns = [
     path('', wishlist_view, name='wishlist'),  # The path for the wishlist view
     path('add/<int:product_id>/', add_to_wishlist, name='add_to_wishlist'),
     path('remove/<int:item_id>/', remove_from_wishlist, name='remove_from_wishlist'),
+    path('combined/', combined_view, name='combined_view'),
 ]
