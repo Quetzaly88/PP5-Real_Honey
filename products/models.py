c8511bf9e3d2f9eca474acf85c64738e7ff10db7
Andrea Nilsson <andreaycoral@icloud.com>
2025-02-10 22:43:09 +0000

    Enhanced product detail page

--- products/models.py
+++ products/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/', blank=True, null=True) #make image optional
-    
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    season = models.CharField(max_length=100, blank=True, null=True)    
+    flavor_tones = models.CharField(max_length=255, blank=True, null=True)  
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image_1 = models.ImageField(upload_to='products/', blank=True, null=True)
+    image_2 = models.ImageField(upload_to='products/', blank=True, null=True)
+    image_3 = models.ImageField(upload_to='products/', blank=True, null=True)
+
     def __str__(self):
         return self.name
