b0521124292316c648a138822c547bd43147624a
Andrea Nilsson <andreaycoral@icloud.com>
2025-02-10 17:52:01 +0000

    Add a combined_view to show both wishlist and shopping cart items

--- wishlist/views.py
+++ wishlist/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import WishlistItem
 from products.models import Product 
+from shopping_cart.models import CartItem 
 
 # Create your views here.
 @login_required
@@ -27,3 +28,21 @@ def remove_from_wishlist(request, item_id):
 def login_redirect_view(request):
     messages.info(request, "Please log in to save products to your wishlist.")
     return redirect('account_login')
+
+@login_required
+def combined_view(request):
+    #Fetch wishlist and cart items for the logged-in user
+    wishlist_items = WishlistItem.objects.filter(user=request.user)
+    cart_items = CartItem.objects.filter(user=request.user)
+    
+    #Calculate the total price of shopping cart items
+    total_price = sum(item.get_total_price() for item in cart_items)
+    
+    return render(
+        request, 
+        'wishlist/combined.html', 
+        {'wishlist_items': wishlist_items, 
+         'cart_items': cart_items,
+         'total_price': total_price,
+        }
+    )
