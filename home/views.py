2c9c3bb40334da68f84e3134439c7ca469af0ff4
Andrea Nilsson <andreaycoral@icloud.com>
2025-02-18 09:50:56 +0000

    Create html and view for featured products

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from products.models import Product
+
 
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+
+def home_view(request):
+    featured_products = Product.objects.order_by('?')[:3]
+    return render(request, 'home/home.html', {
+        'featured_products': featured_products})
