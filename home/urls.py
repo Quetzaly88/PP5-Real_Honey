a44aa12e01d0420c821d6982196bad842c7af7db
Andrea Nilsson <andreaycoral@icloud.com>
2025-02-25 08:23:43 +0000

    Add style in featured and best seller products in home. Debug views

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,9 @@
 # from django.contrib import admin
 from django.urls import path
-from . import views
+# from . import views
 from .views import home_view
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    # path('', views.index, name='home'),
     path('', home_view, name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -11,7 +11,7 @@ def home_view(request):
     featured_products = Product.objects.filter(
         is_featured=True).order_by('?')[:3]
     best_seller_products = Product.objects.filter(
-        is_bestseller=True).order_by('?')[:3]
+        is_best_seller=True).order_by('?')[:3]
 
     return render(request, 'home/index.html', {
         'featured_products': featured_products,
