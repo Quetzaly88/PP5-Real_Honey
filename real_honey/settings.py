7cb6d4478c09714926ccbed0e8b360056aee325b
Andrea Nilsson <andreaycoral@icloud.com>
2025-03-12 12:19:58 +0000

    Uninstall allauth and install an older version "allauth==0.50.0". Make changes in settings and migrate. Erase allauth files

--- real_honey/settings.py
+++ real_honey/settings.py
@@ -76,8 +76,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # Add the account middleware:
-    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'real_honey.urls'
